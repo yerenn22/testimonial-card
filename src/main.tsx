60a3992d5f45d98a61299adc4433face7f09218d
Yeren Kalibek <yerenn22@gmail.com>
2025-01-14 17:51:59 +0500

    fix: add missed fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./app";
 
+import "@fontsource/noto-sans/latin-400.css";
+import "@fontsource/noto-sans/latin-600.css";
+
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
